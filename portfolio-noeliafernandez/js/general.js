d05bebcef02d482b0c926fe3b00d1c33a984751e
NoeliaLurromi <soylurromi@alumnos.cei.es>
2023-10-25 18:54:31 +0200

    Intentando resolver errores

--- portfolio-noeliafernandez/js/general.js
+++ portfolio-noeliafernandez/js/general.js
@@ -14,3 +14,6 @@ console.log( headerNav )
 headerButton.addEventListener( `click` , () => {
     headerNav.classList.toggle(`isActive`)
 } )
+
+// https://es.stackoverflow.com/questions/516192/cambiar-un-valor-de-una-propiedad-css-desde-js
+// Hay que cambiar la propiedad display block por none , mirar es página
