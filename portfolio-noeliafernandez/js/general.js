9bfdfdf5439c5e5824ef047c62b5e14f0041dcbe
NoeliaLurromi <soylurromi@alumnos.cei.es>
2023-10-29 20:47:27 +0100

    Transiciones entre páginas / Cambios en header durante el scroll / Corrección de errores

--- portfolio-noeliafernandez/js/general.js
+++ portfolio-noeliafernandez/js/general.js
@@ -15,5 +15,49 @@ headerButton.addEventListener( `click` , () => {
     headerNav.classList.toggle(`isActive`)
 } )
 
-// https://es.stackoverflow.com/questions/516192/cambiar-un-valor-de-una-propiedad-css-desde-js
-// Hay que cambiar la propiedad display block por none , mirar es página
+const header = document.querySelector(`.Header`)
+
+
+
+// COMPORTAMIENTO DEL HEADER //
+    // Cuando hago MOUSEWHEEL hacia abajo (DELTAY +) el header le ADD la class .isHidden
+    // Cuando hago scroll hacia arriba (DELTAY -) el header le REMOVE la class .isHidden
+
+window.addEventListener("mousewheel" , (e) => {
+    console.log(e.deltaY)
+    if(e.deltaY >= 0){
+        // menú desaparece
+        header.classList.add(`isHidden`)
+    } else{
+        // menu aparece
+        header.classList.remove(`isHidden`)
+    }
+})
+
+
+// TRANSICIÓN ENTRE PÁGINAS //
+
+const enlaces = document.querySelectorAll(`.Header-a`)
+const cortinillaNaranja = document.querySelector(`.Cortinilla-naranja`)
+const cortinillaSalmon = document.querySelector(`.Cortinilla-salmon`)
+
+console.log( enlaces )
+console.log( cortinillaNaranja)
+console.log( cortinillaSalmon)
+
+enlaces.forEach(( eachEnlace , i) => {
+    enlaces[i].addEventListener(`click` , ( e ) => {
+        e.preventDefault()
+
+        const href = enlaces[i].href
+        
+        cortinillaNaranja.classList.add(`isActive`)
+        setTimeout(() => {
+            window.location = href 
+        } ,1000)
+    })
+})
+
+
+
+
